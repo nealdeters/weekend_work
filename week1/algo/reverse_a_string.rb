b7662ae759d1547a7d790fc20c4bb3dc7d1e0351
nealdeters <nealdeters@gmail.com>
2016-01-08 12:15:52 -0600

    completed reverse_a_string

--- week1/algo/reverse_a_string.rb
+++ week1/algo/reverse_a_string.rb
@@ -1,6 +1,6 @@
 # Complete the method called reverse_a_string that accepts a string as a parameter and 
 # returns the reverse. The one caveat: Don't use the reverse method that already
-# comes with Ruby!
+# comes with Ruby
 
 def reverse_a_string(string)
   string_length = string.length
