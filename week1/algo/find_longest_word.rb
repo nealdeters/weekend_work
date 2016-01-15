05117ce3ad1222425661f1fbc9f8cfdf91887082
nealdeters <nealdeters@gmail.com>
2016-01-15 14:50:20 -0600

    finished all weekend work, but batman vs superman

--- week1/algo/find_longest_word.rb
+++ week1/algo/find_longest_word.rb
@@ -3,22 +3,37 @@
 def find_longest_word(sentence)
   longest_word = ""
   words = sentence.split(" ")
+  
   words.each do |word|
     #new_word = word
-    if word.include?("?")
-      word.delete! "?"
+    # if word.include?("?")
+    #   word.delete! "?"
+    # end
+    just_letters_string = word.gsub(/\W/, "")
+    test_string = longest_word.gsub(/\W/, "")
+    word_array = word.split("")
+
+    correct_characters = ""
+
+    word_array.each do |character|
+      correct_characters += character if character =~ /(\w|['])/
     end
 
-    longest_word = word unless word.length < longest_word.length
+    #longest_word = word unless word.length < longest_word.length
+    if just_letters_string.length > test_string.length
+      longest_word = correct_characters
+    end
   end
+
   longest_word
+
 end
 
 # Driver code - don't touch anything below this line.
 puts "TESTING find_longest_word..."
 puts
 
-result = find_longest_word("What is the longest word in this phrase?")
+result = find_longest_word("What is the longest word, that couldn't be in this phrase? or string")
 
 puts "Your method returned:"
 puts result
