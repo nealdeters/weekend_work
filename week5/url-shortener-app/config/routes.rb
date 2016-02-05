28b4ca1cc1ad18ba74e86c0d5162736296025883
nealdeters <nealdeters@gmail.com>
2016-02-04 21:23:27 -0600

    created url shortener app

---
+++ week5/url-shortener-app/config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  devise_scope :user do
+   authenticated :user do
+     root 'links#index', as: :authenticated_root
+   end
+
+   unauthenticated do
+     root 'devise/registrations#new', as: :unauthenticated_root
+   end
+ end
+ 
+end
