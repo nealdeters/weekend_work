20691f6bf04e92f660cb711b9890982bd2a77dc2
nealdeters <nealdeters@gmail.com>
2016-02-06 19:13:41 -0600

    finalized url_shortener

---
+++ week5/url-shortener-app/app/models/link.rb
@@ -0,0 +1,17 @@
+class Link < ActiveRecord::Base
+  has_many :visits
+  belongs_to :user
+
+  validates :slug, presence: true
+  validates :target_url, presence: true
+
+  def standardize_target_url!
+    self.target_url.gsub!("http://", "")
+    self.target_url.gsub!("https://", "")
+  end
+
+  def visit_count
+    self.visits.count
+  end
+
+end
--- week5/url-shortener-app/config/routes.rb
+++ week5/url-shortener-app/config/routes.rb
@@ -2,15 +2,21 @@ Rails.application.routes.draw do
   devise_for :users
 
   devise_scope :user do
-   authenticated :user do
-     root 'links#index', as: :authenticated_root
-   end
+    authenticated :user do
+      root 'links#index', as: :authenticated_root
+    end
 
-   unauthenticated do
-     root 'devise/registrations#new', as: :unauthenticated_root
-   end
- end
+    unauthenticated do
+      root 'devise/registrations#new', as: :unauthenticated_root
+    end
+  end
 
- get '/links' => 'links#index'
+  resources :links
+
+  resources :visits
+
+  get '/:slug' => 'visits#create'
+
+
 
 end
